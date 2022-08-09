0db8bb0bac0178b3134c1ed17f1c6884f69cfde0
BuildTools <unconfigured@null.spigotmc.org>
2022-08-09 14:54:35 +0200

    duck

---
+++ duck.js
@@ -0,0 +1,17 @@
+function getCursorPosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    return [x, y]
+}
+var img = document.getElementById('img')
+const canvas = document.querySelector('canvas')
+var scale = 2;
+canvas.width = 10000;
+canvas.height = 10000;
+canvas.addEventListener('mousedown', function(e) {
+    var x = getCursorPosition(canvas, e)
+    console.log(x)
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(img, x[0]-50, x[1]-40, 100, 80);
+})
